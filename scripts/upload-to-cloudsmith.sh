75d1cb38c6793bf36060f0b95a942e192a658600
Gayan Perera <gayanper@gmail.com>
2022-07-24 20:53:42 +0200

    fix cli secrets and package version

--- scripts/upload-to-cloudsmith.sh
+++ scripts/upload-to-cloudsmith.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-VERSION=$(echo $1 | awk -F"-" '{ print $2}' | awk -F".vsix" '{ print $1}')
+VERSION=$(echo $1 | awk -F"-" '{ print $NF}' | awk -F".vsix" '{ print $1}')
 echo "## Uploading $1 with the version $VERSION ##"
 cloudsmith push raw gap/vsce $1 --version $VERSION --republish 
