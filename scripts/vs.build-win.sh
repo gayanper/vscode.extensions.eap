c12681195710ad1f713b3c1bde15b27a4afd02d3
Gayan Perera <gayanper@gmail.com>
2022-09-06 21:55:29 +0200

    change to zip

--- scripts/vs.build-win.sh
+++ scripts/vs.build-win.sh
@@ -3,6 +3,6 @@ cd ./vscode
 
 yarn && \
 yarn gulp compile-build && yarn gulp compile-extension-media && yarn gulp compile-extensions-build && yarn gulp minify-vscode && \ 
-yarn gulp "vscode-win32-x64-min-ci" && yarn gulp "vscode-win32-x64-inno-updater" && yarn gulp "vscode-win32-x64-user-setup"
+yarn gulp "vscode-win32-x64-min-ci" && yarn gulp "vscode-win32-x64-archive"
 
 cd ..
