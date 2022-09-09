8b177fe477386cf0fc19222d866856ac8b229df7
Gayan Perera <gayanper@gmail.com>
2022-09-09 09:54:50 +0200

    Update build-vscode-java.sh

--- scripts/build-vscode-java.sh
+++ scripts/build-vscode-java.sh
@@ -3,6 +3,8 @@
 echo "## Building vscode-java"
 cd ./extensions/vscode-java
 
+rm -f package-lock.json
+
 cp -f ../../target-files/org.eclipse.jdt.ls.tp.target ../eclipse.jdt.ls/org.eclipse.jdt.ls.target && \
 npm install && npm run build-server && vsce package
 
