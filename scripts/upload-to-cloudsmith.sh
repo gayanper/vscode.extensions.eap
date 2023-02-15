e2c5cd7071c26672698ec05fd34779725a9c6d53
Gayan Perera <gayanper@gmail.com>
2023-02-15 21:37:39 +0100

    unique version

--- scripts/upload-to-cloudsmith.sh
+++ scripts/upload-to-cloudsmith.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 VERSION=$(echo $1 | awk -F"-" '{ print $NF}' | awk -F".vsix" '{ print $1}')
+TIME=$(date +%y%m%d_%H%M%S)
+VERSION="$VERSION-$TIME"
 echo "## Uploading $1 with the version $VERSION ##"
 cloudsmith push raw gap/vsce $1 --version $VERSION --republish 
