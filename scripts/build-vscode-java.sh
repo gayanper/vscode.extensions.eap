e188c154e74d28c3d98ad6d681f945d9b89ecc1b
Gayan Perera <gayanper@gmail.com>
2023-02-16 19:35:25 +0100

    include lombok and jre

--- scripts/build-vscode-java.sh
+++ scripts/build-vscode-java.sh
@@ -5,7 +5,10 @@ cd ./extensions/vscode-java
 
 rm -f package-lock.json
 
-npm install && npm run build-server && ./../../scripts/patch-jdt.ls.sh && vsce package
+npm install && npm run build-server && ./../../scripts/patch-jdt.ls.sh \
+&& npx gulp download_lombok && \
+npx gulp download_jre --target darwin-arm64 --javaVersion 17 \
+&& vsce package --pre-release
 
 cd ./../../
 echo "## Building vscode-java done"
