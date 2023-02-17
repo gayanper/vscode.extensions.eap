c275ba8f402fefc26569fc6a37603ea422d7991c
Gayan Perera <gayanper@gmail.com>
2023-02-17 22:18:21 +0100

    improve version number

--- scripts/build-vscode-java.sh
+++ scripts/build-vscode-java.sh
@@ -5,10 +5,13 @@ cd ./extensions/vscode-java
 
 rm -f package-lock.json
 
+version=$(grep version -m1 package.json | sed 's/.*"version": "\(.*\)".*/\1/')
+version="$version-$(date +%y%m%d_%H%M%S)"
+
 npm install && npm run build-server && ./../../scripts/patch-jdt.ls.sh \
 && npx gulp download_lombok && \
 npx gulp download_jre --target darwin-arm64 --javaVersion 17 \
-&& vsce package --pre-release
+&& vsce package -o "java-$version.vsix"
 
 cd ./../../
 echo "## Building vscode-java done"
--- scripts/upload-to-cloudsmith.sh
+++ scripts/upload-to-cloudsmith.sh
@@ -1,6 +1,4 @@
 #!/bin/bash
 VERSION=$(echo $1 | awk -F"-" '{ print $NF}' | awk -F".vsix" '{ print $1}')
-TIME=$(date +%y%m%d_%H%M%S)
-VERSION="$VERSION-$TIME"
 echo "## Uploading $1 with the version $VERSION ##"
 cloudsmith push raw gap/vsce $1 --version $VERSION --republish 
