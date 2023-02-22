2ef4791985aefee8cca76a3cd23463e10a09f59f
Gayan Perera <gayanper@gmail.com>
2023-02-22 21:32:24 +0100

    Add timestamp release version support

--- scripts/build-vscode-java.sh
+++ scripts/build-vscode-java.sh
@@ -6,12 +6,14 @@ cd ./extensions/vscode-java
 rm -f package-lock.json
 
 version=$(grep version -m1 package.json | sed 's/.*"version": "\(.*\)".*/\1/')
-version="$version-$(date +%y%m%d_%H%M%S)"
+current_version=$version
+new_version="${version%.*}.$(date +%y%m%d%H%M%S)"
+sed -i '' "s/${current_version}/${new_version}/" "package.json"
 
 npm install && npm run build-server && ./../../scripts/patch-jdt.ls.sh \
 && npx gulp download_lombok && \
 npx gulp download_jre --target darwin-arm64 --javaVersion 17 \
-&& vsce package -o "java-$version.vsix"
+&& vsce package -o "java-$new_version.vsix"
 
 cd ./../../
 echo "## Building vscode-java done"
