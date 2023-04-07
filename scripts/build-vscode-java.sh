f42db320ed5b17dc6fe4bd4cc2c853ff0f09c263
Gayan Perera <gayanper@gmail.com>
2023-04-07 12:19:42 +0200

    remove pre-release tag

--- scripts/build-vscode-java.sh
+++ scripts/build-vscode-java.sh
@@ -8,7 +8,7 @@ rm -f package-lock.json
 npm install && npm run build-server && ./../../scripts/patch-jdt.ls.sh \
 && npx gulp prepare_pre_release && npx gulp download_lombok && \
 npx gulp download_jre --target darwin-arm64 --javaVersion 17 \
-&& vsce package --pre-release
+&& vsce package
 
 cd ./../../
 echo "## Building vscode-java done"
