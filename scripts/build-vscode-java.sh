0e3c687ecfadf5bd9ca22e33ed7220c6e02887b3
Gayan Perera <gayanper@gmail.com>
2023-04-10 22:08:29 +0200

    enable jar patching from eap

--- scripts/build-vscode-java.sh
+++ scripts/build-vscode-java.sh
@@ -5,8 +5,7 @@ cd ./extensions/vscode-java
 
 rm -f package-lock.json
 
-#&& ./../../scripts/patch-jdt.ls.sh
-npm install && npm run build-server \
+npm install && npm run build-server && ./../../scripts/patch-jdt.ls.sh \
 && npx gulp prepare_pre_release && npx gulp download_lombok && \
 npx gulp download_jre --target darwin-arm64 --javaVersion 17 \
 && vsce package
