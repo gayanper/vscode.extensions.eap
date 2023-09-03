87a9e0315d9a3027ff92e0f8ea3b83e561ace23d
Gayan Perera <gayanper@gmail.com>
2023-09-03 15:08:51 +0200

    Fix version replace for java-test extension

--- scripts/build-vscode-java-test.sh
+++ scripts/build-vscode-java-test.sh
@@ -10,10 +10,10 @@ current_version=$version
 new_version="${version%.*}.$(date +%y%m%d%H%M%S)"
 
 # MacOS 
-# sed -i '' "s/${current_version}/${new_version}/" "package.json"
+#sed -i '' "s/ \"${current_version}\"/ \"${new_version}\"/" "package.json"
 
 # Linux
-sed -i "s/$current_version/$new_version/g" "package.json"
+sed -i "s/ \"${current_version}\"/ \"${new_version}\"/g" "package.json"
 
 npm install && npm run build-plugin && vsce package
 
