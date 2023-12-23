f1349a1207187a147581f8eead294418942d2dda
Gayan Perera <gayanper@gmail.com>
2023-12-23 20:20:57 +0100

    remove patch jdt jars

--- scripts/build-vscode-java.sh
+++ scripts/build-vscode-java.sh
@@ -8,14 +8,10 @@ cd ./extensions/vscode-java
 rm -f package-lock.json
 
 # absolute path to the jdt.ls folder
-p2_path=$script_dir"/../jdt-repo-releng/repository/target/repository"
 target_file=$script_dir"/../target-files/org.eclipse.jdt.ls.tp.target"
-p2_path=$(cd -- "$p2_path" >/dev/null 2>&1 ; pwd -P)
-
-#patch_target_file $target_file "JDT_P2" $p2_path
 
 cp -f $target_file $script_dir"/../extensions/eclipse.jdt.ls/org.eclipse.jdt.ls.target/org.eclipse.jdt.ls.tp.target" && \
-npm install && npm run build-server && ./../../scripts/patch-jdt.ls.sh \
+npm install && npm run build-server \
 && npx gulp prepare_pre_release && npx gulp download_lombok && \
 npx gulp download_jre --target darwin-arm64 --javaVersion 17 \
 && vsce package
