93d021e078ba1f7af9d526c6bfe12e48e2032db2
Gayan Perera <gayanper@gmail.com>
2023-12-23 18:56:33 +0100

    update patches

--- scripts/build-vscode-java.sh
+++ scripts/build-vscode-java.sh
@@ -14,7 +14,7 @@ p2_path=$(cd -- "$p2_path" >/dev/null 2>&1 ; pwd -P)
 
 #patch_target_file $target_file "JDT_P2" $p2_path
 
-cp $target_file $script_dir"/../extensions/eclipse.jdt.ls/org.eclipse.jdt.ls.target/org.eclipse.jdt.ls.tp.target" && \
+cp -f $target_file $script_dir"/../extensions/eclipse.jdt.ls/org.eclipse.jdt.ls.target/org.eclipse.jdt.ls.tp.target" && \
 npm install && npm run build-server && ./../../scripts/patch-jdt.ls.sh \
 && npx gulp prepare_pre_release && npx gulp download_lombok && \
 npx gulp download_jre --target darwin-arm64 --javaVersion 17 \
