93c0cf0b95db839e2a6f533f68d3b122625e27fe
Gayan Perera <gayanper@gmail.com>
2024-03-21 19:43:57 +0100

    fix build

--- scripts/build-jdt.sh
+++ scripts/build-jdt.sh
@@ -8,8 +8,8 @@ cd ./extensions/
 git -C ./jdt.core/ pull --no-rebase --no-edit https://github.com/eclipse-jdtls/eclipse-jdt-core-incubator.git master
 
 mvn -f ./jdt.core/ verify -Pbuild-individual-bundles -DskipTests && \
-    mvn -f ./jdt.debug/ verify -Pbuild-individual-bundles -DskipTests && \
-    mvn -f ./jdt.ui/ verify -Pbuild-individual-bundles -DskipTests && \
+#    mvn -f ./jdt.debug/ verify -Pbuild-individual-bundles -DskipTests && \
+#    mvn -f ./jdt.ui/ verify -Pbuild-individual-bundles -DskipTests && \
     mvn -f ../jdt-repo-releng/pom.xml clean verify
 
 cd ./../
