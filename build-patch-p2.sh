5a89f7edf5f6a82231663ffb752426ac3d57b08b
Gayan Perera <gayanper@gmail.com>
2024-04-07 08:40:46 +0200

    add new patches

---
+++ build-patch-p2.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+if [[ "$1" == "--checkout" ]]; then
+    if [ -d ./extensions ]; then
+        rm -Rf ./extensions
+    fi
+    mkdir ./extensions
+
+    echo "## Checkout Phase"
+    ./scripts/checkout-modules.sh
+    echo "## Patch Phase"
+    ./scripts/merge-prs.sh
+fi
+
+echo "## Build Phase"
+./scripts/build-jdt.sh && ./scripts/patch-p2-repo.sh
