221ad8a8c2675f59a792cd7d79087aceeaeb88f9
Gayan Perera <gayanper@gmail.com>
2024-05-11 10:19:14 +0200

    change build params for javac build

--- scripts/build-jdt.sh
+++ scripts/build-jdt.sh
@@ -11,10 +11,17 @@ else
     git -C ./jdt.core/ pull --no-rebase --no-edit https://github.com/eclipse-jdtls/eclipse-jdt-core-incubator.git master
 fi
 
-mvn -f ./jdt.core/ verify -Pbuild-individual-bundles -DskipTests && \
-#    mvn -f ./jdt.debug/ verify -Pbuild-individual-bundles -DskipTests && \
-#    mvn -f ./jdt.ui/ verify -Pbuild-individual-bundles -DskipTests && \
-    mvn -f ../jdt-repo-releng/pom.xml clean verify
+if [[ "$USE_JAVAC_BRANCH" == "true" ]]; then
+    mvn -f ./jdt.core/ verify -DskipTests -Pp2-repo && \
+        mvn -f ./jdt.debug/ verify -Pbuild-individual-bundles -DskipTests && \
+        mvn -f ./jdt.ui/ verify -Pbuild-individual-bundles -DskipTests && \
+        mvn -f ../jdt-repo-releng/pom.xml clean verify
+else
+    mvn -f ./jdt.core/ verify -Pbuild-individual-bundles -DskipTests && \
+        mvn -f ./jdt.debug/ verify -Pbuild-individual-bundles -DskipTests && \
+        mvn -f ./jdt.ui/ verify -Pbuild-individual-bundles -DskipTests && \
+        mvn -f ../jdt-repo-releng/pom.xml clean verify
+fi
 
 cd ./../
 echo "## Building JDT done"
