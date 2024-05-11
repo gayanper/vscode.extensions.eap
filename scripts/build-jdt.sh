9bcdc928cc3ad13ecb71820dc592abf4a52ecb53
Gayan Perera <gayanper@gmail.com>
2024-05-11 10:30:07 +0200

    fix build

--- scripts/build-jdt.sh
+++ scripts/build-jdt.sh
@@ -12,14 +12,14 @@ else
 fi
 
 if [[ "$USE_JAVAC_BRANCH" == "true" ]]; then
-    mvn -f ./jdt.core/ verify -DskipTests -Pp2-repo && \
-        mvn -f ./jdt.debug/ verify -Pbuild-individual-bundles -DskipTests && \
-        mvn -f ./jdt.ui/ verify -Pbuild-individual-bundles -DskipTests && \
+    mvn -f ./jdt.core/ verify -DskipTests -Pp2-repo -Pbuild-individual-bundles && \
+        # mvn -f ./jdt.debug/ verify -Pbuild-individual-bundles -DskipTests && \
+        # mvn -f ./jdt.ui/ verify -Pbuild-individual-bundles -DskipTests && \
         mvn -f ../jdt-repo-releng/pom.xml clean verify
 else
     mvn -f ./jdt.core/ verify -Pbuild-individual-bundles -DskipTests && \
-        mvn -f ./jdt.debug/ verify -Pbuild-individual-bundles -DskipTests && \
-        mvn -f ./jdt.ui/ verify -Pbuild-individual-bundles -DskipTests && \
+        # mvn -f ./jdt.debug/ verify -Pbuild-individual-bundles -DskipTests && \
+        # mvn -f ./jdt.ui/ verify -Pbuild-individual-bundles -DskipTests && \
         mvn -f ../jdt-repo-releng/pom.xml clean verify
 fi
 
