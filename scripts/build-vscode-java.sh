8ad9f051ad8566082e04e88403843eb19c2b507f
Gayan Perera <gayanper@gmail.com>
2024-05-11 14:22:09 +0200

    use target from source for javac

--- scripts/build-vscode-java.sh
+++ scripts/build-vscode-java.sh
@@ -14,10 +14,13 @@ cd ./extensions/vscode-java
 
 rm -f package-lock.json
 
-# absolute path to the jdt.ls folder
-target_file=$script_dir"/../target-files/org.eclipse.jdt.ls.tp.target"
+if [[ "$USE_JAVAC_BRANCH" != "true" ]]; then
+    # only replace target file when building against none javac branch
+    # absolute path to the jdt.ls folder
+    target_file=$script_dir"/../target-files/org.eclipse.jdt.ls.tp.target"
+    cp -f $target_file $script_dir"/../extensions/eclipse.jdt.ls/org.eclipse.jdt.ls.target/org.eclipse.jdt.ls.tp.target"
+fi
 
-cp -f $target_file $script_dir"/../extensions/eclipse.jdt.ls/org.eclipse.jdt.ls.target/org.eclipse.jdt.ls.tp.target" && \
 npm install && npm run build-server \
 && npx gulp prepare_pre_release && npx gulp download_lombok && \
 npx gulp download_jre --target darwin-arm64 --javaVersion 17 \
