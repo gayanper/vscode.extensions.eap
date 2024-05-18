4ac166c12d7678090e483e10296fa96a36c13fad
Gayan Perera <gayanper@gmail.com>
2024-05-18 18:54:57 +0200

    improve javac patching

--- scripts/merge-prs.sh
+++ scripts/merge-prs.sh
@@ -5,6 +5,7 @@ source $script_dir/common.sh
 work_dir=$script_dir/../
 repo_file=$work_dir"patches/repos.txt"
 ignore_merge_errors=$1
+extra_patch_suffix=$2
 
 read_repos $repo_file 
 cd $work_dir"extensions"
@@ -22,6 +23,20 @@ do
         do
             cherry_pick_pr $ref $ignore_merge_errors
         done
+    fi
+
+    # add extra patches
+    if [ -z "$extra_patch_suffix" ]; then
+        extra_key=$repo_key"."$extra_patch_suffix".txt
+
+        if [ -f $work_dir"/patches/"$extra_key".txt" ]; then
+            echo "Applying $extra_key PRs"
+            read_refs $work_dir"/patches/"$extra_key".txt"
+            for ref in "${read_refs_val[@]}"
+            do
+                cherry_pick_pr $ref $ignore_merge_errors
+            done
+        fi
     fi
 
     # apply patch files
