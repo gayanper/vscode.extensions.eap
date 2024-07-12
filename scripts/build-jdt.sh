fedf3061ea33c7c75b7b59340cd7d832e3f8fa6b
Gayan Perera <gayanper@gmail.com>
2024-07-12 08:56:06 +0200

    remove releng build from javac

--- scripts/build-jdt.sh
+++ scripts/build-jdt.sh
@@ -6,10 +6,10 @@ echo "## Building JDT"
 cd ./extensions/
 
 if [[ "$USE_JAVAC_BRANCH" == "true" ]]; then
-    mvn -f ./jdt.core/ install -DskipTests -Pp2-repo && \
+    mvn -f ./jdt.core/ install -DskipTests -Pp2-repo # && \
         # mvn -f ./jdt.debug/ verify -Pbuild-individual-bundles -DskipTests && \
         # mvn -f ./jdt.ui/ verify -Pbuild-individual-bundles -DskipTests && \
-        mvn -f ../jdt-repo-releng/pom.xml clean verify
+        # mvn -f ../jdt-repo-releng/pom.xml clean verify
 else
     mvn -f ./jdt.core/ verify -Pbuild-individual-bundles -DskipTests && \
         # mvn -f ./jdt.debug/ verify -Pbuild-individual-bundles -DskipTests && \
