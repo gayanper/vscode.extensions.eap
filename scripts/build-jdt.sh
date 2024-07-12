50bf4a6f6b3e03c6020c4c162ce337f1bf4a3b71
Gayan Perera <gayanper@gmail.com>
2024-07-12 09:53:05 +0200

    improve build scripts

--- scripts/build-jdt.sh
+++ scripts/build-jdt.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+set -e
 script_dir=$(cd -- "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P)
 source $script_dir/common.sh
 
@@ -6,10 +7,10 @@ echo "## Building JDT"
 cd ./extensions/
 
 if [[ "$USE_JAVAC_BRANCH" == "true" ]]; then
-    mvn -f ./jdt.core/ install -DskipTests -Pp2-repo # && \
+    mvn -f ./jdt.core/ install -DskipTests -Pp2-repo && \
         # mvn -f ./jdt.debug/ verify -Pbuild-individual-bundles -DskipTests && \
         # mvn -f ./jdt.ui/ verify -Pbuild-individual-bundles -DskipTests && \
-        # mvn -f ../jdt-repo-releng/pom.xml clean verify
+        mvn -f ../jdt-repo-releng/pom.xml clean verify
 else
     mvn -f ./jdt.core/ verify -Pbuild-individual-bundles -DskipTests && \
         # mvn -f ./jdt.debug/ verify -Pbuild-individual-bundles -DskipTests && \
