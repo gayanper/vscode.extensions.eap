a96cb0717a540fb8e21552ea81ccc8b11c4f4292
Gayan Perera <gayanper@gmail.com>
2024-07-14 11:40:36 +0200

    update patches and optimized the javac build.

--- scripts/build-jdt.sh
+++ scripts/build-jdt.sh
@@ -7,7 +7,9 @@ echo "## Building JDT"
 cd ./extensions/
 
 if [[ "$USE_JAVAC_BRANCH" == "true" ]]; then
-    mvn -f ./jdt.core/ install -DskipTests -Pp2-repo && \
+    mvn -f ./jdt.core/ \
+        -pl '!org.eclipse.jdt.core.tests.builder,!org.eclipse.jdt.core.tests.compiler,!org.eclipse.jdt.core.tests.javac,!org.eclipse.jdt.core.tests.model,!org.eclipse.jdt.core.tests.performance,!org.eclipse.jdt.compiler.tool.tests,!org.eclipse.jdt.compiler.apt.tests,!org.eclipse.jdt.apt.ui,!org.eclipse.jdt.apt.tests,!org.eclipse.jdt.apt.pluggable.tests' \
+        install -DskipTests -Pp2-repo && \
         # mvn -f ./jdt.debug/ verify -Pbuild-individual-bundles -DskipTests && \
         # mvn -f ./jdt.ui/ verify -Pbuild-individual-bundles -DskipTests && \
         mvn -f ../jdt-repo-releng/pom.xml clean verify
