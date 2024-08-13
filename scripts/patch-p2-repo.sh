ab3e61ff30f1ebfdc52603d3d83388a8bae9a4cd
Gayan Perera <gayanper@gmail.com>
2024-08-13 06:28:39 +0200

    Update patch-p2-repo.sh

--- scripts/patch-p2-repo.sh
+++ scripts/patch-p2-repo.sh
@@ -8,24 +8,24 @@ echo "## Patching P2 Repository"
 # then patch files and remove checksums again check the repo to make sure its still valid
 
 ln -s $PWD/jdt-repo-releng/target/repository-jdtincu $PWD/jdt-repo-releng/target/repository && \
-    mvn -f ./jdt-repo-releng/pom.xml org.eclipse.tycho:tycho-p2-repository-plugin:4.0.4:verify-repository && rm $PWD/jdt-repo-releng/target/repository && \
+    mvn -f ./jdt-repo-releng/pom.xml org.eclipse.tycho:tycho-p2-repository-plugin:4.0.8:verify-repository && rm $PWD/jdt-repo-releng/target/repository && \
 
     ln -s $PWD/jdt-repo-releng/target/repository-eclipse $PWD/jdt-repo-releng/target/repository && \
-    mvn -f ./jdt-repo-releng/pom.xml org.eclipse.tycho:tycho-p2-repository-plugin:4.0.4:verify-repository && rm $PWD/jdt-repo-releng/target/repository&& \
+    mvn -f ./jdt-repo-releng/pom.xml org.eclipse.tycho:tycho-p2-repository-plugin:4.0.8:verify-repository && rm $PWD/jdt-repo-releng/target/repository&& \
 
     /usr/bin/python3 $script_dir/patch-p2-repository.py && \
 
     ln -s $PWD/jdt-repo-releng/target/repository-jdtincu $PWD/jdt-repo-releng/target/repository && \
-    mvn -f ./jdt-repo-releng/pom.xml org.eclipse.tycho:tycho-p2-repository-plugin:4.0.4:fix-artifacts-metadata && rm $PWD/jdt-repo-releng/target/repository && \
+    mvn -f ./jdt-repo-releng/pom.xml org.eclipse.tycho:tycho-p2-repository-plugin:4.0.8:fix-artifacts-metadata && rm $PWD/jdt-repo-releng/target/repository && \
 
     ln -s $PWD/jdt-repo-releng/target/repository-eclipse $PWD/jdt-repo-releng/target/repository && \
-    mvn -f ./jdt-repo-releng/pom.xml org.eclipse.tycho:tycho-p2-repository-plugin:4.0.4:fix-artifacts-metadata && rm $PWD/jdt-repo-releng/target/repository && \
+    mvn -f ./jdt-repo-releng/pom.xml org.eclipse.tycho:tycho-p2-repository-plugin:4.0.8:fix-artifacts-metadata && rm $PWD/jdt-repo-releng/target/repository && \
 
     ln -s $PWD/jdt-repo-releng/target/repository-jdtincu $PWD/jdt-repo-releng/target/repository && \
-    mvn -f ./jdt-repo-releng/pom.xml org.eclipse.tycho:tycho-p2-repository-plugin:4.0.4:verify-repository && rm $PWD/jdt-repo-releng/target/repository && \
+    mvn -f ./jdt-repo-releng/pom.xml org.eclipse.tycho:tycho-p2-repository-plugin:4.0.8:verify-repository && rm $PWD/jdt-repo-releng/target/repository && \
 
     ln -s $PWD/jdt-repo-releng/target/repository-eclipse $PWD/jdt-repo-releng/target/repository && \
-    mvn -f ./jdt-repo-releng/pom.xml org.eclipse.tycho:tycho-p2-repository-plugin:4.0.4:verify-repository &&  rm $PWD/jdt-repo-releng/target/repository
+    mvn -f ./jdt-repo-releng/pom.xml org.eclipse.tycho:tycho-p2-repository-plugin:4.0.8:verify-repository &&  rm $PWD/jdt-repo-releng/target/repository
 
 echo "## Patching P2 Repository done"
 
